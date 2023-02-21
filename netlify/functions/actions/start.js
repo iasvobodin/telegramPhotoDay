6666bc3f498073e4b2e7e5682288778e1136defc
iasvobodin <iasvobodin@gmail.com>
2023-02-21 16:31:50 +0500

    kang

--- netlify/functions/actions/start.js
+++ netlify/functions/actions/start.js
@@ -26,7 +26,7 @@ module.exports = async ctx => {
                        first_name: "${ctx.from.first_name}"
                        last_name: "${ctx.from.last_name}"
                        username: "${ctx.from.username}"
-                       language_code: "${ctx.from.username}"
+                       language_code: "${ctx.from.language_code}"
                    }){
                         id
                     }
