129577ce013075443ce965f91e2babecb2054d45
iasvobodin <iasvobodin@gmail.com>
2023-02-21 17:30:58 +0500

    ee

--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -15,21 +15,6 @@ exports.handler = async function (event, context) {
     } catch (error) {
         console.log(error);
     }
-    // send a message to the chat acknowledging receipt of their message
-    //   });
-
-    // const bot = new Telegraf(process.env.BOT_TOKEN);
-    // bot.telegram.sendMessage("576118532", "сообщеня юхуху");
-
-    // bot.on('message', function (ctx, next) {
-    //     ctx.telegram.sendMessage(576118532,
-    //         "сообщеня юхуху"
-    //     )
-    // });
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify({ message: "sending" }),
-    // };
 };
 
 
