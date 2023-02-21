03f43cf926396ba3975cef6ebb538e8728e58dae
iasvobodin <iasvobodin@gmail.com>
2023-02-21 20:31:08 +0500

    bot

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -2,25 +2,58 @@
 // const startAction = require('./actions/start')
 
 import { Telegraf } from "telegraf";
-import startAction from "./actions/start"
+// import startAction from "./actions/start"
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-// ctx.from {
-//     id: 576118532,
-//         is_bot: false,
-//             first_name: 'Ivan',
-//                 last_name: 'Svobodin',
-//                     username: 'iasvobodin',
-//                         language_code: 'ru'
-// }
-bot.start(ctx => {
-    console.log(ctx.message.chat, 'chat', ctx.message.from, 'CTX FROM');
-    return startAction(ctx)
+
+bot.start(async ctx => {
+    if (ctx.from.is_bot) {
+        return ctx.reply(`Sorry I only interact with humans!`)
+    }
+
+    try {
+        await fetch('https://graphql.fauna.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.FAUNA_SECRET_KEY}`
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation CreateUser {
+                        createUser(data: {
+                           id: "${ctx.from.id}" 
+                           is_bot: ${ctx.from.is_bot}
+                           first_name: "${ctx.from.first_name}"
+                           last_name: "${ctx.from.last_name}"
+                           username: "${ctx.from.username}"
+                           language_code: "${ctx.from.language_code}"
+                       }){
+                            id
+                        }
+                     }
+                `,
+            }),
+        })
+        return ctx.reply(`Added ${ctx.from.username} to db!`)
+        // const result = await response.json();
+        //     let isNewUser = await newUser(id)
+        //     if (isNewUser) {
+        //         return ctx.reply(`Added ${name} to db!`)
+        //     } else {
+        //         return ctx.reply(`${name} is already inside db!`)
+        //     }
+
+    } catch (e) {
+        console.log(e);
+        return ctx.reply(`Error occured`)
+    }
 })
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
 exports.handler = async event => {
+    console.log(event.body, "event.body");
     try {
         await bot.handleUpdate(JSON.parse(event.body));
+        bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
         return { statusCode: 200, body: '' };
     } catch (e) {
         console.log(e)
--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -1,13 +1,12 @@
-import TelegramBot from 'node-telegram-bot-api'
+// import TelegramBot from 'node-telegram-bot-api'
 import { Telegraf } from "telegraf";
 
 exports.handler = async function (event, context) {
-    const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-    const bot2 = new Telegraf(process.env.BOT_TOKEN);
+    // const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+    const bot = new Telegraf(process.env.BOT_TOKEN);
 
     try {
-        await bot.sendMessage(397739262, 'Женуля красотуля, люблю тебя!!!');
-        await bot2.telegram.sendMessage(576118532, "сообщеня юхуху");
+        await bot.telegram.sendMessage(576118532, "сообщеня юхуху");
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "sending" }),
