6aa2be090ed499f7614a40f697df1df8c9afa9e4
iasvobodin <iasvobodin@gmail.com>
2023-02-21 16:42:32 +0500

    ss

--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -3,6 +3,11 @@ import { Telegraf } from "telegraf";
 exports.handler = async function (event, context) {
     const bot = new Telegraf(process.env.BOT_TOKEN);
     bot.telegram.sendMessage(576118532, "сообщеня юхуху");
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "sending" }),
+    };
 };
 
 
