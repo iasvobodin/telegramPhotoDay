3b640ffcb24d9bc50ec1ff9371a55c2282f0042b
iasvobodin <iasvobodin@gmail.com>
2023-02-21 17:29:04 +0500

    22

--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -1,25 +1,13 @@
 import TelegramBot from 'node-telegram-bot-api'
-
-
-
-
-
-
-
-
-// import { Telegraf } from "telegraf";
+import { Telegraf } from "telegraf";
 
 exports.handler = async function (event, context) {
-    // Create a bot that uses 'polling' to fetch new updates
     const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-
-
-
-    // bot.on('message', (msg) => {
-    // const chatId = msg.chat.id;
+    const bot2 = new Telegraf(process.env.BOT_TOKEN);
 
     try {
         await bot.sendMessage(397739262, 'Женуля красотуля, люблю тебя!!!');
+        await bot2.telegram.sendMessage(576118532, "сообщеня юхуху");
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "sending" }),
