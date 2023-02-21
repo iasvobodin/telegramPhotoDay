86712842df3f1ed7d4ec56ef649fc7df8181d6d3
iasvobodin <iasvobodin@gmail.com>
2023-02-21 21:27:20 +0500

    fffffix

--- netlify/functions/actions/start.js
+++ netlify/functions/actions/start.js
@@ -1,6 +1,12 @@
+// const { newUser } = require('../components/fauna')
+import { getUser } from "../components/helper";
+
 module.exports = async ctx => {
-    console.log('start');
-    if (ctx.from.is_bot) {
+    // console.log(ctx.from, 'ctx.from');
+    const { id, isBot, name } = getUser(ctx.from)
+    // console.log(id, isBot, name, 'from start.js')
+
+    if (isBot) {
         return ctx.reply(`Sorry I only interact with humans!`)
     }
 
@@ -29,6 +35,13 @@ module.exports = async ctx => {
             }),
         })
         return ctx.reply(`Added ${ctx.from.username} to db!`)
+        // const result = await response.json();
+        //     let isNewUser = await newUser(id)
+        //     if (isNewUser) {
+        //         return ctx.reply(`Added ${name} to db!`)
+        //     } else {
+        //         return ctx.reply(`${name} is already inside db!`)
+        //     }
 
     } catch (e) {
         console.log(e);
