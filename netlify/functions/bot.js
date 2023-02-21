7aa01684bdbf5cd38424c6b6f520647878933d2e
iasvobodin <iasvobodin@gmail.com>
2023-02-21 14:56:52 +0500

    ctx

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -5,9 +5,16 @@ import { Telegraf } from "telegraf";
 import startAction from "./actions/start"
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
+// ctx.from {
+//     id: 576118532,
+//         is_bot: false,
+//             first_name: 'Ivan',
+//                 last_name: 'Svobodin',
+//                     username: 'iasvobodin',
+//                         language_code: 'ru'
+// }
 bot.start(ctx => {
-    console.log(ctx, 'CTX', ctx.from, 'CTX FROM');
+    console.log(ctx.chat, 'CTX', ctx.from, 'CTX FROM');
     return startAction(ctx)
 })
 
