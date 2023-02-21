56f59e4bcd41d5d6b4b5c9eb3ca5bcec6568b897
iasvobodin <iasvobodin@gmail.com>
2023-02-21 17:15:48 +0500

    new api

--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -1,18 +1,47 @@
-import { Telegraf } from "telegraf";
+import { TelegramBot } from 'node-telegram-bot-api'
+
+
+
+
+
+
+
+
+// import { Telegraf } from "telegraf";
 
 exports.handler = async function (event, context) {
-    const bot = new Telegraf(process.env.BOT_TOKEN);
-    bot.telegram.sendMessage("576118532", "сообщеня юхуху");
+    // Create a bot that uses 'polling' to fetch new updates
+    const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-    bot.on('message', function (ctx, next) {
-        ctx.telegram.sendMessage(576118532,
-            "сообщеня юхуху"
-        )
-    });
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "sending" }),
-    };
+
+
+    // bot.on('message', (msg) => {
+    // const chatId = msg.chat.id;
+
+    try {
+        await bot.sendMessage(576118532, 'Received your message');
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "sending" }),
+        };
+    } catch (error) {
+        console.log(error);
+    }
+    // send a message to the chat acknowledging receipt of their message
+    //   });
+
+    // const bot = new Telegraf(process.env.BOT_TOKEN);
+    // bot.telegram.sendMessage("576118532", "сообщеня юхуху");
+
+    // bot.on('message', function (ctx, next) {
+    //     ctx.telegram.sendMessage(576118532,
+    //         "сообщеня юхуху"
+    //     )
+    // });
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ message: "sending" }),
+    // };
 };
 
 
